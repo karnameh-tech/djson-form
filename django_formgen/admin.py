da1a52e31d1a3ff2f33c5746b29e911b2f036d9c
Mohsen Khodabakhshi <gophersen@proton.me>
2025-08-10 11:26:54 +0330

    init commit

---
+++ django_formgen/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django_formgen.models import JSONSchema, TestModel
+from django_formgen.forms import JSONSchemaForm
+from django_formgen.views import dynamic_form_view
+from django.urls import path
+
+
+class JSONSchemaAdmin(admin.ModelAdmin):
+    form = JSONSchemaForm
+    list_display = ('name', 'slug', 'created_at', 'updated_at')
+    search_fields = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                '<path:object_slug>/view/',
+                self.admin_site.admin_view(dynamic_form_view),
+                name='json_schema_test',
+            ),
+        ]
+        return custom_urls + urls
+
+
+admin.site.register(JSONSchema, JSONSchemaAdmin)
+admin.site.register(TestModel)
